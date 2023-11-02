d26f460aa47d83537da95735cbd420970988cf32
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-11-02 23:33:05 +0530

    dashboard

--- Company-Dashboard/src/Company-Dashboard/dashboard/CompanyDashboard.jsx
+++ Company-Dashboard/src/Company-Dashboard/dashboard/CompanyDashboard.jsx
@@ -1,13 +1,57 @@
 import React from "react";
 
+import { FaHome, FaMapMarkedAlt } from "react-icons/fa";
+import {FaLocationCrosshairs} from 'react-icons/fa6'
+import { IoIosPeople } from "react-icons/io";
+
 import "./companyDashboard.scss";
-import CompanySidebar from "../../components/sidebar/CompanySidebar";
 
 const CompanyDashboard = () => {
   return (
-    // <CompanySidebar>
-      <div className="CompanyDashboard">CompanyDashboard</div>
-    // </CompanySidebar> 
+    <div className="CompanyDashboard">
+      <div>
+        <div className="hello">Hello! Ritav </div>
+        <div className="welcome">Welcome to the Dashboard </div>
+      </div>
+      <div className="cards">
+        <div className="cardBox">
+          <div className="text">
+            <div>Number of Tracks</div>
+            <div>12</div>
+          </div>
+          <div className="icon">
+            <FaMapMarkedAlt />
+          </div>
+        </div>
+        <div className="cardBox">
+          <div className="text">
+            <div>Number of Users</div>
+            <div>12</div>
+          </div>
+          <div className="icon">
+            <IoIosPeople />
+          </div>
+        </div>
+        <div className="cardBox">
+          <div className="text">
+            <div>Number of Locations</div>
+            <div>12</div>
+          </div>
+          <div className="icon">
+            <FaLocationCrosshairs />
+          </div>
+        </div>
+        <div className="cardBox">
+          <div className="text">
+            <div>Number of tracks</div>
+            <div>12</div>
+          </div>
+          <div className="icon">
+            <FaHome />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
